cc124e153873eb6a627d958ef28225989545208c
Alex Rog <sasharg303@gmail.com>
2023-11-23 15:13:32 +0100

    make build

---
+++ index.91bc5562.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=e.offsetWidth/2,c=document.querySelector(".wall").clientWidth/2;e.style.top="".concat(c-t,"px"),e.style.left="".concat(c-t,"px");
+//# sourceMappingURL=index.91bc5562.js.map
